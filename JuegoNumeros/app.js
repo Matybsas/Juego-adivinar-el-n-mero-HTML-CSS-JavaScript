fa21795f7927623bed3b1709d81b08dd791c18c1
Matias <126203934+Matybsas@users.noreply.github.com>
2023-11-09 02:57:40 -0300

    Mejoramos el codigo app.js

--- JuegoNumeros/app.js
+++ JuegoNumeros/app.js
@@ -2,13 +2,15 @@
 let numeroAzar = Math.floor(Math.random() * 100) + 1;
 /*Tomamos el valor ingresado por el usuario */
 let numeroUsuario = document.getElementById('numeroUsuario');
-/*Enlazamos con el texto que va aparecer luego */
+/*Enlazamos con el parrafo que va a mostrar un mensaje despues */
 let mensaje = document.getElementById('mensajeUsuario');
 /*Tomamos el nombre ingresado por el usuario */
 let nombre = document.getElementById('nombre');
-
+/*Enlazamos el parrafo con el numero a mostrar posteriormente */
+let numeroGanador=document.getElementById('numeroGanador');
 /*Creamos un contador para contar los intentos */
-let contador = 0;
+let contador = 1;
+/*Inicializamos el contador de intentos a mostrar */
 let contadorIntentos = 1;
 /*Ponemos un contador de intentos */
 let intentosRealizados= document.getElementById('intentosRealizados');
@@ -21,17 +23,17 @@ function verificarResultado() {
     let numUser = parseInt(numeroUsuario.value);
 
     /*creamos un for para controlar los intentos */
-    if (contador < 4) {
+    if (contador <= 5) {
         intentosRealizados.textContent= `Llevas ${contadorIntentos} intentos`;
         contadorIntentos++;
         if(numUser<1 || numUser>100 || isNaN(numUser)){
             mensaje.textContent ='Por favor ingresa un numero valido del 0 al 100';
-            mensaje.style.color='black';
+            mensaje.style.color='red';
             return;
         }
 
         if (numUser === numeroAzar) {
-            mensaje.textContent = `¡¡¡Felicidades ${nombre.value}! Has acertado,eres un Genio!!! `;
+            mensaje.textContent = `¡¡¡Felicidades ${nombre.value}! Has acertado el numero ganador era el: ${numeroAzar},eres un Genio/a!!! `;
             mensaje.style.color = 'greenyellow';
         }
         else if (numUser < numeroAzar) {
@@ -46,12 +48,15 @@ function verificarResultado() {
         return contador++;
     } else {
 
-        mensaje.textContent = "Superaste la cantidad de intentos!!!";
+        mensaje.textContent = "Perdiste, superaste la cantidad de 5 intentos!!!";
+        numeroGanador.textContent=`El numero ganador era el: ${numeroAzar}`
         mensaje.style.color = 'black';
         numeroUsuario.disabled = true
     }
 
 }
+
+
 
 
 
